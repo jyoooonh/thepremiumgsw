64b7473e9255232612049e64fe1658f31dc07ae3
Jungyoon Han <jyoon0c11@gmail.com>
2023-03-04 15:52:31 +0900

    url 수정

--- qna/urls.py
+++ qna/urls.py
@@ -6,6 +6,6 @@ app_name = 'qna'
 
 urlpatterns = [
 	path('', views.index,name='index'),
-	path('<int:teacher_id>/', views.detail, name="detail"),
+	path('/<int:teacher_id>/', views.detail, name="detail"),
 	path('reserve/', views.reserve, name='reserve'),
 ]
