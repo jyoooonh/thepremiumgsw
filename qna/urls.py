acc5e9389198d5f25d89ed282185b5da20b21a43
Jungyoon Han <jyoon0c11@gmail.com>
2023-03-04 16:44:38 +0900

    gitignore 적용

--- qna/urls.py
+++ qna/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'qna'
 
 urlpatterns = [
-	path('', views.index,name='index'),
+	path('', views.index, name='index'),
 	path('<int:teacher_id>/', views.detail, name="detail"),
 	path('reserve/', views.reserve, name='reserve'),
 ]
