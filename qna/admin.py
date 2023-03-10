cf040e5e8fe9c85d864255f26174741fa68fcefa
Jungyoon Han <jyoon0c11@gmail.com>
2023-03-10 09:28:44 +0900

    chore/도메인 추가

--- qna/admin.py
+++ qna/admin.py
@@ -3,6 +3,8 @@ from .models import Teacher, Time_Table, Reserve
 
 class ReserveAdmin(admin.ModelAdmin):
     list_display = ('date', 'teacher_id', 'time')
+    #search_fields = ["teacher_id", "student_name", "date"]
+
 
 admin.site.register(Reserve, ReserveAdmin)
 admin.site.register(Teacher)
