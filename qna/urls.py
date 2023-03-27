0e2cb7f95d7fb3981f1e5e12e5e5377895e1560d
Jungyoon Han <jyoon0c11@gmail.com>
2023-03-27 12:21:32 +0900

    chore/스케줄 변경

--- qna/urls.py
+++ qna/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'qna'
 
 urlpatterns = [
-	path('', views.index, name='index'),
+	#path('', views.index, name='index'),
 	path('<int:teacher_id>/', views.detail, name="detail"),
 	path('reserve_complete/<int:reserve_id>', views.reserve, name='reserve_complete'),
 	path('reserve_delete/<int:reserve_id>', views.reserve_delete, name='reserve_delete')
