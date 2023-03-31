99c9f1a08ba3e865bffa5e17801d03b4fdff7b21
Jungyoon Han <jyoon0c11@gmail.com>
2023-03-31 12:39:12 +0900

    chore:코멘트 내용 보이도록

--- qna/admin.py
+++ qna/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Teacher, Time_Table, Reserve
 
 class ReserveAdmin(admin.ModelAdmin):
-    list_display = ('date', 'time', 'teacher_id', 'student_name', 'comment_subject')
+    list_display = ('date', 'time', 'teacher_id', 'student_name', 'comment_subject', "comment_content")
     search_fields = ["teacher_id__name", "student_name__username", "date"]
 
 admin.site.register(Reserve, ReserveAdmin)
