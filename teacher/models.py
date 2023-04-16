0d2791bb03a42df4bbca43c94ebb44de86e910c7
Jungyoon Han <jyoon0c11@gmail.com>
2023-04-16 21:53:57 +0900

    feat/상담 내용 등록, 폼 개선

--- teacher/models.py
+++ teacher/models.py
@@ -5,12 +5,11 @@ class Consulting(models.Model):
     id = models.BigAutoField(primary_key=True)
     date = models.DateField(null=True)
     subject = models.CharField(max_length=10, null=True, blank=True)
-    teacher_name = models.ForeignKey(User, on_delete=models.CASCADE)
+    teacher_name = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     student_name = models.CharField(max_length=10, null=True, blank=True)
     consulting_type = models.CharField(max_length=20, null=True, blank=True)
     consulting_subject = models.CharField(max_length=100, null=True, blank=True)
     consulting_content = models.TextField(max_length=2000, null=True, blank=True)
-    create_date = models.DateTimeField(null=True)
     
     def __str__(self):
         return self.student_name
--- teacher/views.py
+++ teacher/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render
 
 from qna.models import Teacher, Reserve
 from .models import Consulting
+from .forms import ConsultingForm
 
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.decorators import user_passes_test
 
 from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
 from django.core.paginator import Paginator
 
 
@@ -16,10 +18,15 @@ def index(request):
 
 @user_passes_test(lambda u: u.is_staff, login_url='common:login')
 def main(request):
-	consulting = Consulting.objects.order_by('-create_date')
-	teacher = Teacher.objects.order_by('id')
-	context = {'teacher': teacher, 'consulting': consulting}
-	return render(request, 'teacher/teacher_page.html', context)
+    consulting = Consulting.objects.order_by('-id')
+    teacher = Teacher.objects.order_by('id')
+        
+    page = request.GET.get('page', '1')
+    paginator = Paginator(consulting, 15)
+    page_obj = paginator.get_page(page)
+
+    context = {'teacher': teacher, 'consulting': page_obj}
+    return render(request, 'teacher/teacher_page.html', context)
 
 
 @login_required(login_url='common:login')
@@ -31,8 +38,24 @@ def consulting_detail(request, consulting_id):
 
 @login_required(login_url='common:login')
 def consulting_create(request):
+    if request.method == 'POST':
+        form = ConsultingForm(request.POST)
+        if form.is_valid():
+            consulting = form.save(commit=False)
+            consulting.teacher_name = request.user
+            consulting_type = form.cleaned_data.get('consulting_type')
+            if consulting_type == 'other':
+                other_consulting_type = form.cleaned_data.get('other_consulting_type')
+                consulting.consulting_type = other_consulting_type
+            else:
+                consulting.consulting_type = consulting_type
+            consulting.save()
+            return redirect('teacher:main')
+    else:
+        form = ConsultingForm()
+    context = {'form': form}
+    return render(request, 'teacher/consulting_form.html', context)
 
-	return render(request, 'teacher/consulting_form.html')
 
 
 @login_required(login_url='common:login')
